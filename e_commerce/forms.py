754bb29363fda53a54babb76dc0efbdd14276319
henrique <hgphema@gmail.com>
2020-02-09 15:01:10 -0300

    add register and login form and template

--- e_commerce/forms.py
+++ e_commerce/forms.py
@@ -1,8 +1,12 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+# Formulário de Contato
 class ContactForm(forms.Form):
-    nome_completo = forms.CharField(
-    error_messages={'required': 'Obrigatório o preenchimento do nome'},
+
+    full_name = forms.CharField(
         widget=forms.TextInput(
             attrs={
                     "class": "form-control",
@@ -10,8 +14,8 @@ class ContactForm(forms.Form):
                 }
             )
         )
+
     email     = forms.EmailField(
-        error_messages={'invalid': 'Digite um email válido!'},
         widget=forms.EmailInput(
             attrs={
                     "class": "form-control",
@@ -19,12 +23,52 @@ class ContactForm(forms.Form):
                 }
             )
         )
-    mensagem   = forms.CharField(
-        error_messages={'required': 'É obrigatório o preenchimento do campo mensagem!'},
+
+    content   = forms.CharField(
         widget=forms.Textarea(
             attrs={
                     "class": "form-control",
                     "placeholder": "Digite sua mensagem"
                 }
             )
-)
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("O Email deve ser do gmail.com")
+        return email
+
+# Formulário de Login
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+# Formulário de Registro
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        qs = User.objects.filter(username=username)
+        if qs.exists():
+            raise forms.ValidationError("Esse usuário já existe, escolha outro nome.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("Esse email já existe, tente outro!")
+        return email
+
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+        if password != password2:
+            raise forms.ValidationError("As senhas informadas devem ser iguais!")
+        return data
