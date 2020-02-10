605f83fd5f1dc8722461bd9a8c4c3c71a5bb8a67
henrique <hgphema@gmail.com>
2020-02-10 20:33:16 -0300

    insert __str__ method in models.py

--- products/models.py
+++ products/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model): #product_category
+
+class Product(models.Model):
+    # Atributos dos produtos
     title       = models.CharField(max_length=120)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+
+    def __str__(self):
+        return self.title
