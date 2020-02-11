487e8abfc1d79cd8d22bb05a3af5eec6c48da956
henrique <hgphema@gmail.com>
2020-02-11 13:52:04 -0300

    change views.py

--- products/views.py
+++ products/views.py
@@ -1,14 +1,23 @@
-from django.views.generic import ListView
-from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
-#Class Based View
+#Class Based View List
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada
     queryset = Product.objects.all()
     template_name = "products/list.html"
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+#Class Based View Detail
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args,**kwargs)
         print(context)
         return context
