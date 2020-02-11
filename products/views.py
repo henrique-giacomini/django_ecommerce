f8f4e26b303cae8a27bd57f8088681704b849b50
henrique <hgphema@gmail.com>
2020-02-11 20:20:48 -0300

    insert get_object method in product/views.py

--- products/views.py
+++ products/views.py
@@ -14,10 +14,17 @@ class ProductListView(ListView):
 
 #Class Based View Detail
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args,**kwargs)
         print(context)
         return context
+
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
