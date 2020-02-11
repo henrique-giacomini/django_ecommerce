ff502a0306c4c42f0a9fbfee063b727901c4b5bf
henrique <hgphema@gmail.com>
2020-02-11 20:26:00 -0300

    import Http404

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Product
 
 #Class Based View List
