6c969059beb66097f1e00a38b30d1a2c9a348812
henrique <hgphema@gmail.com>
2020-02-10 22:15:23 -0300

    update products template

--- products/views.py
+++ products/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
 from django.views.generic import ListView
+from django.shortcuts import render
+
 from .models import Product
 
-# Create your views here.
-
+#Class Based View
 class ProductListView(ListView):
+    #traz todos os produtos do banco de dados sem filtrar nada
     queryset = Product.objects.all()
     template_name = "products/list.html"
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
