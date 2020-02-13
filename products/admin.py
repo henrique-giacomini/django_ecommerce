3c0761a8becf5b224d57d8c444a60ed683178d16
henrique <hgphema@gmail.com>
2020-02-13 17:26:03 -0300

    create ProductAdmin in products/admin.py and register it

--- products/admin.py
+++ products/admin.py
@@ -3,4 +3,10 @@ from .models import Product
 
 # Register your models here.
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('__str__','slug')
+    class meta:
+        model = Product
+
+
+admin.site.register(Product, ProductAdmin)
