d54aee83b93822a8c22e0e63121146580218bb7e
henrique <hgphema@gmail.com>
2020-02-17 20:10:13 -0300

    insert timestamp in models.py

--- products/models.py
+++ products/models.py
@@ -51,12 +51,13 @@ class ProductManager(models.Manager):
 # Create your models here.
 class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
-    slug        = models.SlugField(blank = True, unique = True)
+    slug        = models.SlugField(blank=True, unique=True)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
-    image       = models.ImageField(upload_to = upload_image_path, null = True, blank = True)
-    featured     = models.BooleanField(default = False)
-    active     = models.BooleanField(default = True)
+    image       = models.ImageField(upload_to=upload_image_path, null = True, blank = True)
+    featured    = models.BooleanField(default=False)
+    active      = models.BooleanField(default=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
 
     objects     = ProductManager()
 
